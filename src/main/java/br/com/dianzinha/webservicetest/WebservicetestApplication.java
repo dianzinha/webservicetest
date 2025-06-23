ac2d3f04b477aece7904ff2e1a311a0578d936b7
Caroline Gardeli Dian <carolinegardeli@hotmail.com>
2025-06-23 16:29:23 -0300

    Criando a estrutura do projeto de weebservice para estudo

---
+++ src/main/java/br/com/dianzinha/webservicetest/WebservicetestApplication.java
@@ -0,0 +1,13 @@
+package br.com.dianzinha.webservicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dianzinha/webservicetest/WebservicetestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dianzinha.webservicetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
