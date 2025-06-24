d32237cdf841a733a3321e54e46d5245d3d2ddab
Caroline Gardeli Dian <carolinegardeli@hotmail.com>
2025-06-24 17:35:45 -0300

    Criando metodos de GET all e GET by id na controller

--- src/main/java/br/com/dianzinha/webservicetest/controllers/ProductController.java
+++ src/main/java/br/com/dianzinha/webservicetest/controllers/ProductController.java
@@ -4,13 +4,16 @@ import br.com.dianzinha.webservicetest.dtos.ProductRecordDto;
 import br.com.dianzinha.webservicetest.models.ProductModel;
 import br.com.dianzinha.webservicetest.repositories.ProductRepository;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.spi.ObjectThreadContextMap;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -25,5 +28,18 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getProductById (@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productObject = productRepository.findById(id);
+        if(productObject.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(productObject.get());
+    }
 
 }
