ef88abe793b251720c0ed37deb0837cdf1e6a8eb
Caroline Gardeli Dian <carolinegardeli@hotmail.com>
2025-06-24 18:07:22 -0300

    Criaçao do metodo DELETE na controller

--- src/main/java/br/com/dianzinha/webservicetest/controllers/ProductController.java
+++ src/main/java/br/com/dianzinha/webservicetest/controllers/ProductController.java
@@ -54,4 +54,14 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productObject = productRepository.findById(id);
+        if(productObject.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(productObject.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
+
 }
